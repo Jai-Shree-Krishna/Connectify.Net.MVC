767b67eb796ff4e9f8f5ab7524a66fa117c74acb
HariHar <sanjaytataria@gmail.com>
2023-10-13 12:36:14 +0530

    added comment model

---
+++ MVC_1/Models/Post.cs
@@ -0,0 +1,35 @@
+﻿using MVC_1.Models;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace MVC_1.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; } // Assuming an integer ID, you can change this to match your database setup
+
+        [Required]
+        public int UserId { get; set; } // Foreign key to User
+
+        [ForeignKey("UserId")]
+        public required User User { get; set; } // Navigation property for User
+
+        [Required]
+        public required string Title { get; set; }
+
+        public required string Description { get; set; }
+
+        [Required]
+        public required string Image { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public int Likes { get; set; } = 0;
+
+        public int Dislikes { get; set; } = 0;
+    }
+}
+
+
---
+++ MVC_1/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using ServiceStack.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_1.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; } // Assuming an integer ID, you can change this to match your database setup
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public required string FirstName { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public required string LastName { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [Unique] // You might need to implement unique constraints based on your database
+        public required string UserName { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public required string Email { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public required string Password { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public required string Gender { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now; // Setting the default value
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public DateTime Dob { get; set; }
+
+        public int Age { get; set; }
+
+        public byte[] Image { get; set; }
+    }
+}
